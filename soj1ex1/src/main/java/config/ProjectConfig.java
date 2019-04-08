c09921bdc7678db36bf77a0677e698269811d4ba
Your Name <you@example.com>
2019-04-08 19:58:26 +0300

    sdsa

--- soj1ex1/src/main/java/config/ProjectConfig.java
+++ soj1ex1/src/main/java/config/ProjectConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProjectConfig {
     @Bean   // am creat instanta si am pus-o in context
+    @Scope("prototype") //asta ca sa nu faca aceeasi referinta, foloseste ca sa obtin mai multe referinte in main
     public Cat cat(){   //metoda asta reprezinta instanta cu totul, ea este instanta, nu face ceva
         Cat c = new Cat();
         c.name = "Tom";
